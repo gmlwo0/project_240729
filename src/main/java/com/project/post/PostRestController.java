3f2f4a2235fb3d75222c9b5258cccdc5a12ed1c7
DESKTOP-O7E5F2L\user <test@test.com>
2024-08-02 18:39:07 +0900

    PostRestController

---
+++ src/main/java/com/project/post/PostRestController.java
@@ -0,0 +1,10 @@
+package com.project.post;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/post")
+@RestController
+public class PostRestController {
+
+}
