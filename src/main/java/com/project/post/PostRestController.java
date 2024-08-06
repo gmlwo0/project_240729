82052ce78d0a6a854a625ba914eb2009f38d2497
DESKTOP-QSFTC3I\Administrator <taeci123@naver.com>
2024-08-07 00:27:05 +0900

    08-07 집 post

--- src/main/java/com/project/post/PostRestController.java
+++ src/main/java/com/project/post/PostRestController.java
@@ -1,11 +1,29 @@
 package com.project.post;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.post.bo.PostBO;
 
 @RequestMapping("/post")
 @RestController
 public class PostRestController {
 
+	@Autowired
+	private PostBO postBO;
 	
+	@PostMapping("/create")
+	public Map<String,Object> create(
+			@RequestParam("userId")int userId,
+			@RequestParam("point")double point,
+			@RequestParam("subject")String subject,
+			@RequestParam("content")String content,
+			HttpSession session){
+		
+	}
 }
---
+++ src/main/java/com/project/post/entity/PostEntity.java
@@ -0,0 +1,5 @@
+package com.project.post.entity;
+
+public class PostEntity {
+
+}
