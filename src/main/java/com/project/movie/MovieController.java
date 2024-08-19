3ed2b01e4ae7580fd3d5f16a2c0a126d03cd5af9
DESKTOP-QSFTC3I\Administrator <taeci123@naver.com>
2024-08-20 00:34:09 +0900

    movie검색

--- src/main/java/com/project/movie/MovieController.java
+++ src/main/java/com/project/movie/MovieController.java
@@ -21,10 +21,13 @@ public class MovieController {
 	@Autowired
 	private MovieBO movieBO;
 
-//	@GetMapping("/movie-detail-view")
-//	public String movieDetailView() {
-//		return "movie/movieDetail";
-//	}
+	@GetMapping("/movie-detail-view")
+	public String movieDetailView(
+			Model model,HttpSession session) {
+		List<Movie> movieList = movieBO.getMovieList();
+		model.addAttribute("movieList", movieList);
+		return "movie/movieDetail";
+	}
 
 	@GetMapping("/movie-list-view")
 	public String movieListView(
@@ -32,12 +35,13 @@ public class MovieController {
 			Model model,HttpSession session) {
 		List<Movie> movieList = movieBO.getMovieList();
 		model.addAttribute("movieList", movieList);
+		
 		Movie movie = movieBO.getMovieByTitle(keyword);
 		if (movie != null) {
-			// 영화가 존재하면 해당 영화의 리뷰 작성 페이지로 리다이렉트
-			return "redirect:/movie/movie-detail-view?movieTitle=" + movie.getTitle();
+			// 영화가 존재하면 해당 영화의 정보 페이지로
+			return "redirect:/movie-detail-view?movieId=" + movie.getId();
 		} else {
-			// 영화가 존재하지 않으면 다시 목록 페이지로 리다이렉트
+			// 영화가 존재하지 않으면 다시 목록 페이지로
 			return "/movie/movie";
 		}
 	}
